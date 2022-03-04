e3c83cc2aba96f77b49fe2a041e53bd6ea5c5245
Dylan Vautrin <dylan.vautrin@isen.yncrea.fr>
2022-03-04 15:09:21 +0100

    Avancer sur API

--- app/src/main/java/fr/isen/vautrin/androidrestaurant/CategoryActivity.kt
+++ app/src/main/java/fr/isen/vautrin/androidrestaurant/CategoryActivity.kt
@@ -2,30 +2,33 @@ package fr.isen.vautrin.androidrestaurant
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
-
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONException
 
 
 class CategoryActivity : AppCompatActivity() {
+    private var requestQueue: RequestQueue? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_categorie)
-
-        val img = intent.getStringExtra("img")
         val color = intent.getIntExtra("color", Color.GRAY)
         val name = intent.getStringExtra("name")
         val texte = findViewById<TextView>(R.id.text1)
-        val img_asset = findViewById<ImageView>(R.id.Img)
-        val list = findViewById<RecyclerView>(R.id.Liste)
+        val imgasset = findViewById<ImageView>(R.id.Img)
         texte.text=name
         texte.setBackgroundColor(color)
-        img_asset.setImageResource(intent.getIntExtra("img",0))
+        texte.background.alpha=200
+        imgasset.setImageResource(intent.getIntExtra("img",0))
         val toast = Toast.makeText(this, "Vous êtes dans le menu des ".plus(name), Toast.LENGTH_SHORT)
         toast.show()
         // getting the recyclerview by its id
@@ -37,6 +40,17 @@ class CategoryActivity : AppCompatActivity() {
         var arrayprice = intArrayOf(0)
         var arrayimg = arrayOf("")
         val data = ArrayList<ItemViewModel>()
+        ///////API//////////////////////////
+        requestQueue = Volley.newRequestQueue(this)
+        jsonParse()
+
+
+
+        ////////////////////////////////////
+
+
+
+        ////////LISTE EN DUR///////////
         when(name)
         {
             "Entrées" ->{ array = resources.getStringArray(R.array.ListEntree)
@@ -49,7 +63,7 @@ class CategoryActivity : AppCompatActivity() {
               arrayprice = resources.getIntArray(R.array.PriceDesserts)
                 arrayimg = resources.getStringArray(R.array.ImgDesserts)}
         }
-
+        //////////////////////////////////
         // This loop will create 20 Views containing
         // the image with the count of view
         for (i in 0..4) {
@@ -64,4 +78,21 @@ class CategoryActivity : AppCompatActivity() {
 
     }
 
+    private fun jsonParse() {
+        val url = "http://test.api.catering.bluecodegames.com/menu"
+        val request = JsonObjectRequest(Request.Method.GET, url, null, { response ->
+            try {
+                val jsonArray = response.getJSONArray("menu")
+                for (i in 0 until jsonArray.length()) {
+                    val menu =  jsonArray.getJSONObject(i)
+                    val firstvalue = menu.getString("Entrées")
+                    Log.d("CategoryActivity",firstvalue)
+                }
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+        }, { error -> error.printStackTrace() })
+        requestQueue?.add(request)
+    }
 }
+
