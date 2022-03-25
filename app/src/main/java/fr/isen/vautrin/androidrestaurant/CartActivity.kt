afef7daf1ebc733abaccdff30bdeb3e92ec5433f
Dylan Vautrin <dylan.vautrin@isen.yncrea.fr>
2022-03-25 11:54:36 +0100

    App finit manque pastille Panier

---
+++ app/src/main/java/fr/isen/vautrin/androidrestaurant/CartActivity.kt
@@ -0,0 +1,49 @@
+package fr.isen.vautrin.androidrestaurant
+
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import fr.isen.vautrin.androidrestaurant.databinding.ActivityCartBinding
+
+class CartActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCartBinding
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return true
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.listCart.layoutManager = LinearLayoutManager(this)
+        binding.listCart.adapter = CartAdapter(ShoppingCart.getCart(this)){ itCart ->
+            val item  = itCart.copy()
+            item.quantity = -1
+            ShoppingCart.updateCart(item,this)
+           // setupBadge()
+            updatePrice(ShoppingCart.panier.listItem)
+            binding.listCart.adapter?.notifyDataSetChanged()
+
+        }
+        updatePrice(ShoppingCart.panier.listItem)
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun updatePrice(list : MutableList<ItemCart>){
+        if(list.sumOf{(it.price* it.quantity).toDouble()}!=0.0){
+            binding.buyButton.text =  "Commander pour ${list.sumOf{(it.price * it.quantity).toDouble()}} €"
+        }
+        else{
+            binding.buyButton.visibility = View.GONE
+        }
+
+    }
+
+}
--- app/src/main/java/fr/isen/vautrin/androidrestaurant/DetailsActivity.kt
+++ app/src/main/java/fr/isen/vautrin/androidrestaurant/DetailsActivity.kt
@@ -1,11 +1,14 @@
 package fr.isen.vautrin.androidrestaurant
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import fr.isen.vautrin.androidrestaurant.databinding.ActivityDetailsBinding
+
 
 class DetailsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailsBinding
@@ -15,6 +18,9 @@ class DetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailsBinding.inflate(layoutInflater)
         val view = binding.root
+
+        var price: Float = 0F
+        var quantity = 0
         setContentView(view)
         var color = Color.GRAY
         val item = intent.getSerializableExtra("item") as Item
@@ -35,5 +41,37 @@ class DetailsActivity : AppCompatActivity() {
         binding.textView2.text = item.ingredients.joinToString { it.name_fr }
         binding.textView.text =  item.prices.joinToString { it.price+"€" }
 
+        binding.buttonPrice.text = "Ajouter au panier : "+item.prices[0].price + "€"
+
+        binding.buttonPLus.setOnClickListener{
+            quantity++
+            Log.i("quantity",quantity.toString())
+            binding.itemQuantity.text = quantity.toString()
+            price = quantity * item.prices[0].price.toFloat()
+            binding.buttonPrice.text = "Ajouter au panier : "+price.toString()+"€"
+
+        }
+        binding.buttonMoins.setOnClickListener{
+            if(quantity>=1) {
+                quantity--
+            }
+            else
+                quantity=0
+            binding.itemQuantity.text = quantity.toString()
+            price = quantity * item.prices[0].price.toFloat()
+            binding.buttonPrice.text = "Ajouter au panier : "+price.toString()+"€"
+        }
+
+        binding.buttonPanier.setOnClickListener {
+            val intent = Intent(this, CartActivity::class.java)
+            intent.putExtra("item", item)
+            startActivity(intent)
+        }
+
+        binding.buttonPrice.setOnClickListener{
+            ShoppingCart.updateCart(ItemCart(item.images[0],item.name_fr,quantity,item.prices[0].price.toFloat()),this)
+            //setupBadge()
+            Snackbar.make(binding.root,"$quantity ${item.name_fr} bien ajouté au panier", Snackbar.LENGTH_SHORT ).show()
+        }
     }
 }
